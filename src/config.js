7256e22200693a5bb1e33f82da06a63dc3d66985
Ady <ady@GMs-MacBook-Pro.local>
2020-12-17 02:52:08 -0600

    pre deploy

--- src/config.js
+++ src/config.js
@@ -1,4 +1,15 @@
+let apiPath
+let tokenKey
+
+if (process.env.NODE_ENV === 'production') {
+    apiPath = process.env.REACT_APP_API_ENDPOINT,
+    tokenKey = 'blogful-client-auth-token'
+} else {
+    apiPath = 'http://localhost:8000/api',
+    tokenKey = 'blogful-client-auth-token'
+}
+
 export default {
-  API_ENDPOINT: 'http://localhost:8000/api',
-  TOKEN_KEY: 'blogful-client-auth-token',
+    API_ENDPOINT: apiPath,
+    TOKEN_KEY: tokenKey,
 }
